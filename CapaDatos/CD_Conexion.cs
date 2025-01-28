e870b483b5ac15052aa5a9879eadea8d5fdc6f74
Elvis Morales <sys.elvis@gmail.com>
2025-01-27 22:15:10 -0600

    Se creó la clase conexion, para acceder a la base de datos

---
+++ CapaDatos/CD_Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    internal class CD_Conexion
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source=EMORALES\\SQLEXPRESS;Initial Catalog=db_ferreteria;Integrated Security=True;Trust Server Certificate=True");
+
+        public SqlConnection AbrirConexion()
+        {
+            if ( conexion.State == ConnectionState.Closed) 
+            conexion.Open();
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if(conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+
+    }
+}
